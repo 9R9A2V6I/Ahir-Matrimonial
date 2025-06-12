afcbede0356ccc36eb28a21383d33d06485e6638
IPStudio <ravi@ipstudio.co>
2025-06-12 10:10:32 +0530

    SpecialFun-Update

--- src/component/SpecialFun.tsx
+++ src/component/SpecialFun.tsx
@@ -6,7 +6,7 @@ function SpecialFun() {
 
       <div className='w-[90%] flex justify-center items-center flex-col mt-4'>
         <h1 className=' heading lg:text-[55px] text-[25px]  font-semibold '>Special Function For you</h1>
-        <h1 className="heading lg:text-[55px] text-[25px]  font-semibold ">Hello This is new Deployment from CI and CD</h1>
+        <h1 className="heading lg:text-[35px] text-[20px]  font-semibold ">New updated available for you Please check</h1>
       </div></>
   )
 }
